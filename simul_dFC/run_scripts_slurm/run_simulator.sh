5fe74669c4513070883c11ff9cf4ec8485069b93
mtorabi59 <mohammad.torabi@mail.mcgill.ca>
2024-09-19 12:42:52 -0400

    add simul to slurm

---
+++ simul_dFC/run_scripts_slurm/run_simulator.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#SBATCH --job-name=simul_dfc_job   # Optional: Name of your job
+#SBATCH --output=logs/simul_out.txt  # Standard output log
+#SBATCH --error=logs/simul_err.txt   # Standard error log
+#SBATCH --account=rrg-jbpoline           # Account
+#SBATCH --mem=8G                     # Memory request per node
+#SBATCH --array=1-200                # Task array specification
+
+# Activate  virtual environment
+source "/home/mt00/projects/rrg-jbpoline/mt00/venvs/pydfc/bin/activate"
+
+# Run Python script
+python "/home/mt00/projects/rrg-jbpoline/mt00/pydfc/dFC/simul_dFC/task_data_simulator.py"
+
+# Deactivate environment
+deactivate
