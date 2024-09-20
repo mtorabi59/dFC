1dfc2f69875e6aea718e946b4c59786ee4d13b44
mtorabi59 <mohammad.torabi@mail.mcgill.ca>
2024-09-20 12:25:22 -0400

    minor change

--- simul_dFC/run_scripts_slurm/run_simulator.sh
+++ simul_dFC/run_scripts_slurm/run_simulator.sh
@@ -3,15 +3,15 @@
 #SBATCH --job-name=simul_dfc_job   # Optional: Name of your job
 #SBATCH --output=logs/simul_out.txt  # Standard output log
 #SBATCH --error=logs/simul_err.txt   # Standard error log
-#SBATCH --account=rrg-jbpoline           # Account
+#SBATCH --account=def-jbpoline           # Account
 #SBATCH --mem=8G                     # Memory request per node
 #SBATCH --array=1-200                # Task array specification
 
 # Activate  virtual environment
-source "/home/mt00/projects/rrg-jbpoline/mt00/venvs/pydfc/bin/activate"
+source "/home/mt00/pydfc/bin/activate"
 
 # Run Python script
-python "/home/mt00/projects/rrg-jbpoline/mt00/pydfc/dFC/simul_dFC/task_data_simulator.py"
+python "/home/mt00/pydfc/dFC/simul_dFC/task_data_simulator.py"
 
 # Deactivate environment
 deactivate
