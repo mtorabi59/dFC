72c564b901f5f40d15db42a41669abf4d4655dea
mtorabi59 <mohammad.torabi@mail.mcgill.ca>
2024-12-05 12:26:43 -0500

    minor

--- task_dFC/run_scripts_slurm/run_FCS.sh
+++ task_dFC/run_scripts_slurm/run_FCS.sh
@@ -4,8 +4,7 @@
 #SBATCH --output=logs/fcs_out.txt  # Standard output log
 #SBATCH --error=logs/fcs_err.txt   # Standard error log
 #SBATCH --time=7-00:00:00                # Walltime for each task (7 days)
-#SBATCH --mem-per-cpu=64G                # Memory (64 GB) per cpu
-#SBATCH --cpus-per-task=8              # Number of CPU cores (increase based on availability)
+#SBATCH --mem=64G                     # Memory request per node
 
 DATASET_INFO="./dataset_info.json"
 METHODS_CONFIG="./methods_config.json"
