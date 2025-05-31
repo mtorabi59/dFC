67535fccd63a3ae4124aa50659ce7413ca1ebb66
mtorabi59 <mohammad.torabi@mail.mcgill.ca>
2025-05-31 17:49:31 -0400

    minor

--- task_dFC/run_scripts_slurm/run_ML.sh
+++ task_dFC/run_scripts_slurm/run_ML.sh
@@ -3,7 +3,6 @@
 #SBATCH --job-name=ML_job   # Optional: Name of your job
 #SBATCH --output=logs/ML_out.txt  # Standard output log
 #SBATCH --error=logs/ML_err.txt   # Standard error log
-#SBATCH --time=4-00:00:00                # Walltime for each task (4 days)
 #SBATCH --mem=128G                     # Memory request per node
 
 DATASET_INFO="./dataset_info.json"
