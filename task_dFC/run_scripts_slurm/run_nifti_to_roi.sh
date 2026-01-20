320adc172014b58fe16acda3358a915afccb7a31
mtorabi59 <mohammad.torabi@mail.mcgill.ca>
2026-01-20 16:17:48 -0500

    add DENOISING_STRATEGY to run_nifti_to_roi.sh

--- task_dFC/run_scripts_slurm/run_nifti_to_roi.sh
+++ task_dFC/run_scripts_slurm/run_nifti_to_roi.sh
@@ -1,24 +1,32 @@
 #!/bin/sh
 #
-#SBATCH --job-name=extract_roi_job   # Optional: Name of your job
-#SBATCH --output=logs/roi_out.txt  # Standard output log
-#SBATCH --error=logs/roi_err.txt   # Standard error log
-#SBATCH --time=24:00:00                # Walltime for each task (24 hours)
-#SBATCH --mem=64G                     # Memory request per node
+#SBATCH --job-name=extract_roi_job
+#SBATCH --output=logs/roi_out.txt
+#SBATCH --error=logs/roi_err.txt
+#SBATCH --time=24:00:00
+#SBATCH --mem=64G
 
+# -----------------------------
+# Inputs
+# -----------------------------
 SUBJECT_LIST="./subj_list.txt"
 DATASET_INFO="./dataset_info.json"
+DENOISING_STRATEGY=${1:-simple}
 
-echo "Number subjects found: `cat $SUBJECT_LIST | wc -l`"
+echo "Denoising strategy: $DENOISING_STRATEGY"
+echo "Number of subjects: $(wc -l < "$SUBJECT_LIST")"
 
-SUBJECT_ID=`sed -n "${SLURM_ARRAY_TASK_ID}p" $SUBJECT_LIST`
+SUBJECT_ID=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "$SUBJECT_LIST")
 echo "Subject ID: $SUBJECT_ID"
 
-# Activate  virtual environment
+# -----------------------------
+# Environment
+# -----------------------------
 source "/home/mt00/venvs/pydfc/bin/activate"
 
 python "/home/mt00/pydfc/dFC/task_dFC/nifti_to_roi_signal.py" \
---dataset_info $DATASET_INFO \
---participant_id $SUBJECT_ID
+    --dataset_info $DATASET_INFO \
+    --participant_id $SUBJECT_ID \
+    --denoising_strategy $DENOISING_STRATEGY
 
 deactivate
